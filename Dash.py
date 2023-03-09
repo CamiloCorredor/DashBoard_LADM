e01608b9347a24a0da85106355254d71f98b6b82
CamiloCorredor <camilocorredorroa@gmail.com>
2023-03-09 15:20:58 -0500

    Cantidad de predios

--- Dash.py
+++ Dash.py
@@ -9,21 +9,32 @@ connection = psycopg2.connect(
 )
 
 cursor = connection.cursor()
+cursor.execute("SELECT COUNT(*) FROM information_schema.schemata where schema_name like 'rev%'")
+schema = cursor.fetchone()[0]
 
-# Ejecutar la consulta SQL para obtener los nombres de los esquemas
+No_poly_SVC = [] 
+No_poly_MAC = []
+No_poly_Uribe = []
+for _ in range(1,schema+1):
+    query = "select count(etiqueta) from rev_0" + str(_) + ".lc_terreno where etiqueta like '18%';"
+    cursor.execute(query)
+    No_poly_SVC.append(cursor.fetchone()[0])
 
-for i in range(1,9):
-    txt = "select count(etiqueta) from rev_0" + str(i) + ".lc_terreno where etiqueta like '50%';"
-    print(txt)
-    cursor.execute(txt)
+    query = "select count(etiqueta) from rev_0" + str(_) + ".lc_terreno where etiqueta like '50%';"
+    cursor.execute(query)
+    No_poly_MAC.append(cursor.fetchone()[0])
+
+    # query1 = "select count(etiqueta) from rev_0" + str(i) + ".lc_terreno where etiqueta like '50370%';"
+    # cursor.execute(query1)
+    # No_poly_MAC.append(cursor.fetchone()[0])
 
 
-    esquemas = cursor.fetchall()
-    print(esquemas)
-# # Imprimir los nombres de los esquemas
 
-# for esquema in esquemas:
-#     print(esquema)
+    
+
+print(No_poly_SVC)
+print(No_poly_MAC)
+
 
 
 
